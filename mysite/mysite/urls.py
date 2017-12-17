d7e5aacf33217e18bc23e9b2be8aa3389b28c1a0
JQ-K <kangqingjie01@gmail.com>
2017-12-17 13:02:09 +0800

    动态视图

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from mysite.views import hello
+from mysite import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^hello/',hello),
+    url(r'^hello/',views.hello),
+    url(r'^time/',views.current_datetime),
 ]
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,4 +1,10 @@
 from django.http import HttpResponse
+import datetime
 
 def hello(request):
     return HttpResponse("Helo world")
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body> It is now {}.</body></html>".format(now) 
+    return HttpResponse(html)
