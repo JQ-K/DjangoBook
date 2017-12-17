6dc14ea00222f16d196c35497bcd3e81998fbc72
JQ-K <kangqingjie01@gmail.com>
2017-12-17 21:55:03 +0800

    动态url

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,5 +20,6 @@ from mysite import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^hello/',views.hello),
-    url(r'^time/',views.current_datetime),
+    url(r'^time/$',views.current_datetime),
+    url(r'^time/plus/(\d{1,2})/$',views.hours_ahead),
 ]
