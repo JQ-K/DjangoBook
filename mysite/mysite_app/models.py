1162eee5d22b002579678e2ebb129b28404d47f6
JQ-K <kangqingjie01@gmail.com>
2017-12-24 13:10:54 +0800

    后台管理设置

--- mysite/mysite_app/models.py
+++ mysite/mysite_app/models.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
@@ -10,14 +11,24 @@ class Publisher(models.Model):
     country = models.CharField(max_length=50)
     website = models.URLField()
 
+    def __str__(self):
+        return self.name
+
 class Author(models.Model):
     first_name = models.CharField(max_length = 30)
     last_name = models.CharField(max_length = 40)
-    email = models.EmailField()
+    email = models.EmailField(blank=True)
+
+    def __str__(self):
+        return '{} {}'.format(self.first_name,self.last_name)
 
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
     authors = models.ManyToManyField(Author)
     publisher = models.ForeignKey(Publisher)
-    publication_date = models.DateField()
+    publication_date = models.DateField(blank=True,null=True)
+
+    def __str__(self):
+        return self.title
+
